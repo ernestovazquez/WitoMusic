80d94d6fbbacd37955febadc9bd46072243490f7
Pepito Pérez <pepito.perez@gmail.com>
2018-05-24 10:34:29 +0200

    He borrado el html  playlist

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ def contact():
 
 @app.route('/search')
 def search():
-    buscador = request.forms.get('buscador')
-    opciones = request.forms.get('opciones')
+    buscador = request.get('buscador')
+    opciones = request.get('opciones')
     datos={"q":buscador,"type":opciones}
     if opciones == "track":
         canciones = requests.get("https://api.spotify.com/v1/search", params=datos)
