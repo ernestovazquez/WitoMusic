0ca20d572ccf52f84dc153c6de904acef11e37e6
Pepito Pérez <pepito.perez@gmail.com>
2018-05-29 17:12:34 +0200

    He modificado el creador

--- app.py
+++ app.py
@@ -124,30 +124,6 @@ def search():
         else:
             return redirect('/spotify')
 
-@app.route('/nueva')
-def nueva():
-    return render_template("segundaplaylists.html")
-
-
-@app.route('/creador', methods=["GET", "POST"])
-def creador():
-    if "token_sp" in session:
-        if token_valido():
-            token=json.loads(session["token_sp"])
-            oauth2 = OAuth2Session(os.environ["client_id"], token=token, scope=scope)
-            nombre = request.form.get('nombre')
-            desc = request.form.get('desc')
-            public = request.form.get('public')
-            headers = {'Accept': 'application/json', 'Content-Type': 'application-json', 'Authorization': 'Bearer ' + session['token_sp']}
-            payload={'name':nombre, 'description':desc, 'public':public}
-            r = oauth2.post('https://api.spotify.com/v1/users/{}/playlists' .format(session["id"]), data=json.dumps(payload), headers=headers)
-            doc=json.loads(r.content.decode("utf-8"))
-            return redirect('/playlist')
-        else:
-            return redirect('/')
-    else:
-        return redirect('/spotify')
-
 
 port=os.environ["PORT"]
 app.run('0.0.0.0',int(port), debug=True)
