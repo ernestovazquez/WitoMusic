99405d714b4f55e00f4dd88008b2e9a20043ace9
Pepito Pérez <pepito.perez@gmail.com>
2018-05-29 16:55:27 +0200

    He modificado el creador

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ def creador():
             r = oauth2.post('https://api.spotify.com/v1/users/{}/playlists' .format(session["id"]), data=json.dumps(payload), headers=headers)
             doc=json.loads(r.content.decode("utf-8"))
             return render_template("creacionplaylist.html", datos=doc, nombre=nombre, desc=desc, public=public)
-            return redirect('/playlist')
+            # return redirect('/playlist')
 
         else:
             return redirect('/')
