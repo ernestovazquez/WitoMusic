58db7a9d241b8acff7999c3fba7334d2538e4a3e
Pepito Pérez <pepito.perez@gmail.com>
2018-06-09 17:42:54 +0200

    He modificado el añadir las canciones  las playlist

--- app.py
+++ app.py
@@ -149,6 +149,7 @@ def añadiraplaylist(uri):
         token=json.loads(session["token_sp"])
         oauth2 = OAuth2Session(os.environ["client_id"], token=token)
         r = oauth2.get('https://api.spotify.com/v1/users/{}/playlists' .format(session["id"]))
+        doc=json.loads(r.content.decode("utf-8"))
         return render_template("seleccionar.html", datos=doc, uri=uri)
     else:
         return redirect('/')
